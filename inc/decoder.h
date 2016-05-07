350d72ec040818372f4d9d31f4dde92898b4d5c4
Oliver Lee <oliverzlee@gmail.com>
2016-05-07 20:12:00 +0200

    Pass av errors to DecoderException constructor
    Add second DecoderException constructor that takes an av error as the
    second argument and append the error string to exception what message.

--- inc/decoder.h
+++ inc/decoder.h
@@ -6,14 +6,23 @@ extern "C" {
 #include "libavformat/avformat.h"
 }
 
+
 class DecoderException: public std::exception {
     public:
-        DecoderException(std::string what_arg) : m_arg(what_arg) { }
+        DecoderException(std::string what_arg) {
+            m_arg = what_arg + "\n";
+        }
+        DecoderException(std::string what_arg, int error_code) {
+            av_strerror(error_code, m_error_buffer, sizeof(m_error_buffer));
+            m_arg = what_arg + " (error '" + m_error_buffer + "')\n";
+        }
         ~DecoderException() throw() { }
         const char* what() const throw() { return m_arg.c_str(); };
     private:
         std::string m_arg;
+        char m_error_buffer[255]; /* use same value as in example */
 };
+
 
 class Decoder {
     public:
@@ -25,5 +34,4 @@ class Decoder {
         AVFormatContext* m_context;
         AVCodecContext* m_codec_context;
         AVCodec* m_codec;
-        char m_error_buffer[255];
 };
--- src/decoder.cc
+++ src/decoder.cc
@@ -3,15 +3,11 @@
 Decoder::Decoder(const char* filename): m_context(nullptr), m_codec_context(nullptr), m_codec(nullptr) {
     int error;
     if ((error = avformat_open_input(&m_context, filename, nullptr, nullptr)) < 0) {
-        av_strerror(error, m_error_buffer, sizeof(m_error_buffer));
-        throw DecoderException(
-                std::string("Error opening file: ") + filename + " (error '" + m_error_buffer + "')\n");
+        throw DecoderException(std::string("Error opening file: ") + filename, error);
     }
     if ((error = avformat_find_stream_info(m_context, nullptr)) < 0) {
         avformat_close_input(&m_context);
-        av_strerror(error, m_error_buffer, sizeof(m_error_buffer));
-        throw DecoderException(
-                std::string("Could not open find stream info (error '") + m_error_buffer + "')\n");
+        throw DecoderException("Could not open find stream info", error);
     }
     for (int i = 0; i < m_context->nb_streams; ++i) {
         m_codec_context = m_context->streams[i]->codec;
@@ -22,15 +18,15 @@ Decoder::Decoder(const char* filename): m_context(nullptr), m_codec_context(null
     }
     if (m_codec_context == nullptr) {
         avformat_close_input(&m_context);
-        throw DecoderException("Could not find audio stream\n");
+        throw DecoderException("Could not find audio stream");
     }
     if (!(m_codec = avcodec_find_decoder((m_codec_context->codec_id)))) {
         avformat_close_input(&m_context);
-        throw DecoderException("Could not find audio codec\n");
+        throw DecoderException("Could not find audio codec");
     }
     if ((error = avcodec_open2(m_codec_context, m_codec, nullptr)) < 0) {
         avformat_close_input(&m_context);
-        throw DecoderException(std::string("Could not open input codec (error '") + m_error_buffer + "')\n");
+        throw DecoderException("Could not open input codec", error);
     }
 }
 
