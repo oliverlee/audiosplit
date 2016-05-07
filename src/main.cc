93b3ec576e29dd8562b31063bd3e46740859e048
Oliver Lee <oliverzlee@gmail.com>
2016-05-07 15:57:10 +0200

    Print number of audio channels in input file
    The number of channels in the first encountered audio stream is used if
    multiple audio channels exist.

--- src/main.cc
+++ src/main.cc
@@ -1,23 +1,49 @@
 #include <iostream>
 
 extern "C" {
-#include "libavcodec/avcodec.h"
+//#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+}
+
+unsigned int count_audio_channels(AVFormatContext* context) {
+    for (int i = 0; i < context->nb_streams; ++i) {
+        AVCodecContext* codec = context->streams[i]->codec;
+        if (codec->codec_type == AVMEDIA_TYPE_AUDIO) {
+            /* Return channel count for first audio type stream */
+            return codec->channels;
+        }
+    }
+    return 0;
 }
 
 int main(int argc, char** argv) {
 
     /* register codecs and formats and other lavf/lavc components*/
-    avcodec_register_all();
+    //avcodec_register_all();
+    av_register_all();
 
     if (argc < 2) {
         std::cout << "Usage: " << argv[0] << " <input_file>\n";
-        std::cout << "Print the number of channels in input file.\n\n.";
+        std::cout << "Print the number of audio channels in input file.\n\n";
         return EXIT_FAILURE;
     }
 
     const char* filename = argv[1];
+    AVFormatContext* context;
+    if (avformat_open_input(&context, filename, nullptr, nullptr) < 0) {
+        std::cout << "Error opening file: " << filename << "\n";
+        return EXIT_FAILURE;
+    }
 
-    std::cout << 3 << " channels in " << filename << "\n";
+    //av_log_set_level(AV_LOG_FATAL);
+    if (avformat_find_stream_info(context, nullptr) < 0) {
+        std::cout << "Error reading stream information\n";
+        avformat_close_input(&context);
+        return EXIT_FAILURE;
+    }
+
+    std::cout << count_audio_channels(context) << " channels in '" << filename << "'\n";
+    avformat_close_input(&context);
 
     return EXIT_SUCCESS;
 }
